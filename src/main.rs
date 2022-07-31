2c2a7ffc07ca32dffffb4f3e5ca54b9619c3cfdb
Stefano Pigozzi <me@steffo.eu>
2022-07-31 08:41:42 +0200

    Create inline bot skeleton

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,23 @@
+use teloxide::prelude::*;
+use teloxide::types::*;
+use log::*;
+
 mod data;
 
-fn main() {
-    println!("Hello, world!");
+
+#[tokio::main]
+async fn main() {
+    pretty_env_logger::init();
+    info!("patched-porobot is starting...");
+
+    let bot = Bot::from_env().auto_send();
+
+    let handler = Update::filter_inline_query().branch(dptree::endpoint(|query: InlineQuery, bot: AutoSend<Bot>| async move {
+        let result = InlineQueryResult::Article(InlineQueryResultArticle::new("test", "Test", InputMessageContent::Text(InputMessageContentText::new("Qui è dove metterei la mia carta, se solo ne avessi una!"))));
+
+        let response = bot.answer_inline_query(&query.id, vec![result]).await;
+        respond(())
+    }));
+
+    Dispatcher::builder(bot, handler).enable_ctrlc_handler().build().dispatch().await;
 }
