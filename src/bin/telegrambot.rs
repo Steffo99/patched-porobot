c945f7e6c572ed5bba70b1d895464c63be72590d
Stefano Pigozzi <me@steffo.eu>
2022-08-07 17:50:09 +0200

    Add branch for `telegrambot` binary without the required feature

--- src/bin/telegrambot.rs
+++ src/bin/telegrambot.rs
@@ -1,6 +1,9 @@
-#![cfg(feature = "telegram")]
+#[cfg(not(feature = "telegram"))]
+fn main() {
+    println!("The `telegram` feature was not included on compilation, therefore this binary is not available.")
+}
 
-
+#[cfg(feature = "telegram")]
 fn main() {
     println!("Hello telegram world!")
 }
