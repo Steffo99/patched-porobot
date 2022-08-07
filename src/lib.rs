64d5b8ef709aad27d0c2ac3b35e0b4c57a7c8121
Stefano Pigozzi <me@steffo.eu>
2022-08-07 17:23:05 +0200

    Uncomment the telegram module

--- src/lib.rs
+++ src/lib.rs
@@ -3,11 +3,14 @@
 //! # Features
 //!
 //! - `search`: Adds a search engine based on [tantivy] for Legends of Runeterra data.
-//! - `telegram`: Adds a [Telegram bot](https://core.telegram.org/bots/api) based on [teloxide] for Legends of Runeterra data.
+//! - `telegram`: Adds a [Telegram bot] based on [teloxide] to send Legends of Runeterra cards in chats.
 //!
 //! # Legal
 //!
 //! [patched_porobot](self) isn't endorsed by Riot Games and doesn't reflect the views or opinions of Riot Games or anyone officially involved in producing or managing Riot Games properties. Riot Games, and all associated properties are trademarks or registered trademarks of Riot Games, Inc.
+//!
+//!
+//! [Telegram bot]: https://core.telegram.org/bots/api
 
 #![warn(missing_docs)]
 #![doc(html_logo_url = "https://raw.githubusercontent.com/Steffo99/patched-porobot/main/icon.png")]
@@ -17,5 +20,5 @@ pub mod data;
 #[cfg(feature = "search")]
 pub mod search;
 
-// #[cfg(feature = "telegram")]
-// pub mod telegram;
+#[cfg(feature = "telegram")]
+pub mod telegram;
