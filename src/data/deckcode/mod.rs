de8f00c8c94107f7d291acd5c80206111dd52766
Stefano Pigozzi <me@steffo.eu>
2022-08-15 23:08:31 +0000

    Start work on DeckCode

--- src/data/deckcode/mod.rs
+++ src/data/deckcode/mod.rs
@@ -2,7 +2,41 @@
 //! 
 //! Adapted from [RiotGames/LoRDeckCodes](https://github.com/RiotGames/LoRDeckCodes) and from [iulianR/lordeckcodes-rs](https://github.com/iulianR/lordeckcodes-rs/blob/master/src/lib.rs).
 
+use crate::data::setbundle::card::Card;
 
+
+/// A deck code, as defined in the [deck codes specification](https://github.com/RiotGames/LoRDeckCodes).
+#[derive(Clone, Debug, PartialEq, Eq, Hash, serde::Serialize, serde::Deserialize)]
+#[serde(transparent)]
 pub struct DeckCode {
-    
+    /// The deck code as a string.
+    pub full: String
+}
+
+
+impl From<String> for DeckCode {
+    fn from(full: String) -> Self {
+        Self { full }
+    }
+}
+
+
+impl From<DeckCode> for String {
+    fn from(dc: DeckCode) -> Self {
+        dc.full
+    }
+}
+
+
+impl From<Vec<(Card, usize)>> for DeckCode {
+    fn from(_: Vec<(Card, usize)>) -> Self {
+        todo!()
+    }
+}
+
+
+impl From<DeckCode> for Vec<(Card, usize)> {
+    fn from(_: DeckCode) -> Self {
+        todo!()
+    }
 }
