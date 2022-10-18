961ab2cea870aac1887f895a890de4199e82fc5f
Stefano Pigozzi <me@steffo.eu>
2022-10-18 15:15:32 +0000

    Allow deck names to be specified in the Telegram bot

--- src/telegram/inline.rs
+++ src/telegram/inline.rs
@@ -43,17 +43,20 @@ pub fn card_to_inlinequeryresult(
     })
 }
 
-/// Convert a [Deck] into a [InlineQueryResult].
-pub fn deck_to_inlinequeryresult(index: &CardIndex, deck: &Deck) -> InlineQueryResult {
+/// Convert a [Deck] with an optional name into a [InlineQueryResult].
+pub fn deck_to_inlinequeryresult(index: &CardIndex, deck: &Deck, name: &Option<&str>) -> InlineQueryResult {
     let code = deck
         .to_code(DeckCodeFormat::F1)
         .expect("serialized deck to deserialize properly");
 
     InlineQueryResult::Article(InlineQueryResultArticle {
         id: format!("{:x}", md5::compute(&code)),
-        title: format!("Deck with {} cards", deck.contents.len()),
+        title: match &name {
+            Some(name) => format!(r#"Deck "{}" with {} cards"#, name, deck.contents.len()),
+            None => format!("Deck with {} cards", deck.contents.len())
+        },
         input_message_content: InputMessageContent::Text(InputMessageContentText {
-            message_text: display_deck(index, deck, code),
+            message_text: display_deck(index, deck, &code, &name),
             parse_mode: Some(ParseMode::Html),
             entities: None,
             disable_web_page_preview: Some(true),
