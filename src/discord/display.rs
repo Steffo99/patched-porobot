b7363e6734774736a984da9825994e5d6721a454
Stefano Pigozzi <me@steffo.eu>
2023-02-06 18:13:39 +0100

    Implement card search

--- src/discord/display.rs
+++
@@ -1,8 +0,0 @@
-use crate::data::corebundle::globals::LocalizedGlobalsIndexes;
-use crate::data::setbundle::card::{Card, CardIndex};
-
-
-/// Render a [`Card`] in Discord Markdown.
-pub fn display_card(globals: &LocalizedGlobalsIndexes, card: &Card) -> String {
-    todo!();
-}
--- src/discord/handler.rs
+++ src/discord/handler.rs
@@ -7,13 +7,11 @@ use serenity::model::prelude::*;
 use serenity::model::application::interaction::*;
 use serenity::model::application::interaction::{InteractionResponseType, Interaction};
 use serenity::model::application::interaction::application_command::CommandDataOptionValue;
-use serenity::model::prelude::interaction::application_command::ApplicationCommandInteraction;
-use tantivy::query::QueryParserError;
-use crate::data::setbundle::card::Card;
-use crate::data::setbundle::keyword::CardKeyword;
+use crate::data::setbundle::r#type::CardType;
 use crate::data::setbundle::rarity::CardRarity;
 use crate::data::setbundle::region::CardRegion;
 use crate::data::setbundle::set::CardSet;
+use crate::data::setbundle::supertype::CardSupertype;
 use crate::search::cardsearch::CardSearchEngine;
 
 pub struct EventHandler;
@@ -52,127 +50,92 @@ impl EventHandler {
                     None => response.content(card.name.clone()),
                 };
                 let response = response.embed(|e| {
-                    e
-                        .title(card.name.clone())
-                        .field("Set", match card.set {
-                            CardSet::Foundations => "<:foundations:1071644734667366410> Foundations",
-                            CardSet::RisingTides => "<:rising_tides:1071644736126976160> Rising Tides",
-                            CardSet::CallOfTheMountain => "<:call_of_the_mountain:1071644738555478076> Call of the Mountain",
-                            CardSet::EmpiresOfTheAscended => "<:empires_of_the_ascended:1071644740342255616> Empires of the Ascended",
-                            CardSet::BeyondTheBandlewood => "<:beyond_the_bandlewood:1071644742640750734> Beyond the Bandlewood",
-                            CardSet::Worldwalker => "<:worldwalker:1071644743798370315> Worldwalker",
-                            CardSet::TheDarkinSaga => "<:the_darkin_saga:1071644746411417610> The Darkin Saga",
-                            CardSet::Events => "Events", // TODO: Add icon
-                            CardSet::Unsupported => "<:invaliddeck:1056022952396730438> Unknown",
-                        }, true)
-                        .field("Regions", card.regions.iter().map(|r| match r {
-                            CardRegion::Noxus => "<:noxus:1056022924169064498> Noxus",
-                            CardRegion::Demacia => "<:demacia:1056023014128484412> Demacia",
-                            CardRegion::Freljord => "<:freljord:1056024331437735936> Freljord",
-                            CardRegion::ShadowIsles => "<:shadowisles:1056022886848135292> Shadow Isles",
-                            CardRegion::Targon => "<:targon:1056022866174418944> Targon",
-                            CardRegion::Ionia => "<:ionia:1056022949569777708> Ionia",
-                            CardRegion::Bilgewater => "<:bilgewater:1056024288215437484> Bilgewater",
-                            CardRegion::Shurima => "<:shurima:1056022884616765500> Shurima",
-                            CardRegion::PiltoverZaun => "<:piltoverzaun:1056022918959734835> Piltover & Zaun",
-                            CardRegion::BandleCity => "<:bandlecity:1056024280493735976> Bandle City",
-                            CardRegion::Runeterra => "<:runeterra:1056022895031238727> Runeterra",
-                            CardRegion::Unsupported => "<:invaliddeck:1056022952396730438> Unknown",
-                        }).join(", "), true)
-                        .color(match card.rarity {
-                            CardRarity::None => 0x000000,
-                            CardRarity::Common => 0x1e6a49,
-                            CardRarity::Rare => 0x244778,
-                            CardRarity::Epic => 0x502970,
-                            CardRarity::Champion => 0x81541f,
-                        })
-                        .field("Rarity", match card.rarity {
+                    e.title(card.name.clone());
+
+                    if card.localized_description_text.len() > 0 {
+                        e.description(card.localized_description_text.clone());
+                    }
+
+                    if card.keywords.len() > 0 {
+                        e.field("Keywords", card.keywords.iter().map(|r|
+                            format!(
+                                "{} {}",
+                                r.discord_emoji(),
+                                r.localized(&engine.globals.keywords).map_or_else(|| String::from("Missing translation"), |l| l.name.clone())
+                            )
+                        ).join(", "), true);
+                    }
+
+                    e.field("Mana cost", format!("{} mana", card.cost), true);
+
+                    if card.r#type == CardType::Unit {
+                        e.field("Stats", format!("{} | {}", card.attack, card.health), true);
+                    }
+
+                    e.field("Types", {
+                        let mut vec = Vec::new();
+                        if card.supertype != CardSupertype::None {
+                            vec.push(String::from(&card.supertype));
+                        }
+                        vec.push(String::from(&card.r#type));
+                        for subtype in card.subtypes.iter() {
+                            vec.push(subtype.to_owned())
+                        };
+                        vec
+                    }.join(", "), true);
+
+                    e.field("Regions", card.regions.iter().map(|r| match r {
+                        CardRegion::Noxus => "<:noxus:1056022924169064498> Noxus",
+                        CardRegion::Demacia => "<:demacia:1056023014128484412> Demacia",
+                        CardRegion::Freljord => "<:freljord:1056024331437735936> Freljord",
+                        CardRegion::ShadowIsles => "<:shadowisles:1056022886848135292> Shadow Isles",
+                        CardRegion::Targon => "<:targon:1056022866174418944> Targon",
+                        CardRegion::Ionia => "<:ionia:1056022949569777708> Ionia",
+                        CardRegion::Bilgewater => "<:bilgewater:1056024288215437484> Bilgewater",
+                        CardRegion::Shurima => "<:shurima:1056022884616765500> Shurima",
+                        CardRegion::PiltoverZaun => "<:piltoverzaun:1056022918959734835> Piltover & Zaun",
+                        CardRegion::BandleCity => "<:bandlecity:1056024280493735976> Bandle City",
+                        CardRegion::Runeterra => "<:runeterra:1056022895031238727> Runeterra",
+                        CardRegion::Unsupported => "<:invaliddeck:1056022952396730438> Unknown",
+                    }).join(", "), false);
+
+                    e.field("Set", match card.set {
+                        CardSet::Foundations => "<:foundations:1071644734667366410> Foundations",
+                        CardSet::RisingTides => "<:rising_tides:1071644736126976160> Rising Tides",
+                        CardSet::CallOfTheMountain => "<:call_of_the_mountain:1071644738555478076> Call of the Mountain",
+                        CardSet::EmpiresOfTheAscended => "<:empires_of_the_ascended:1071644740342255616> Empires of the Ascended",
+                        CardSet::BeyondTheBandlewood => "<:beyond_the_bandlewood:1071644742640750734> Beyond the Bandlewood",
+                        CardSet::Worldwalker => "<:worldwalker:1071644743798370315> Worldwalker",
+                        CardSet::TheDarkinSaga => "<:the_darkin_saga:1071644746411417610> The Darkin Saga",
+                        CardSet::Events => "Events", // TODO: Add icon
+                        CardSet::Unsupported => "<:invaliddeck:1056022952396730438> Unknown",
+                    }, true);
+
+                    e.field("Rarity", match card.supertype {
+                        CardSupertype::Champion => "<:champion:1056024303856001034> Champion",
+                        _ => match card.rarity {
                             CardRarity::None => "None",
                             CardRarity::Common => "<:common:1056024315046412358> Common",
                             CardRarity::Rare => "<:rare:1056022907433799690> Rare",
                             CardRarity::Epic => "<:epic:1056023004028608622> Epic",
                             CardRarity::Champion => "<:champion:1056024303856001034> Champion",
-                        }, true)
-                        // TODO: .field("Types")
-                        .field("Mana cost", format!("{} mana", card.cost), true)
-                        .field("Stats", format!("{} | {}", card.attack, card.health), true)
-                        // TODO
-                        .field("Keywords", card.keywords.iter().map(|r| match r {
-                            CardKeyword::SpellOverwhelm => "SpellOverwhelm",
-                            CardKeyword::Burst => "Burst",
-                            CardKeyword::Countdown => "Countdown",
-                            CardKeyword::OnPlay => "OnPlay",
-                            CardKeyword::Landmark => "Landmark",
-                            CardKeyword::Shurima => "Shurima",
-                            CardKeyword::Attach => "Attach",
-                            CardKeyword::Noxus => "Noxus",
-                            CardKeyword::Fleeting => "Fleeting",
-                            CardKeyword::ClobberNoEmptySlotRequirement => "ClobberNoEmptySlotRequirement",
-                            CardKeyword::Nab => "Nab",
-                            CardKeyword::Focus => "Focus",
-                            CardKeyword::Enlightened => "Enlightened",
-                            CardKeyword::Invoke => "Invoke",
-                            CardKeyword::Boon => "Boon",
-                            CardKeyword::Trap => "Trap",
-                            CardKeyword::Drain => "Drain",
-                            CardKeyword::LastBreath => "LastBreath",
-                            CardKeyword::Demacia => "Demacia",
-                            CardKeyword::BandleCity => "BandleCity",
-                            CardKeyword::Fast => "Fast",
-                            CardKeyword::Bilgewater => "Bilgewater",
-                            CardKeyword::Runeterra => "Runeterra",
-                            CardKeyword::Recall => "Recall",
-                            CardKeyword::Weakest => "Weakest",
-                            CardKeyword::Support => "Support",
-                            CardKeyword::Slow => "Slow",
-                            CardKeyword::Obliterate => "Obliterate",
-                            CardKeyword::Imbue => "Imbue",
-                            CardKeyword::Targon => "Targon",
-                            CardKeyword::ShadowIsles => "ShadowIsles",
-                            CardKeyword::AuraVisualFakeKeyword => "AuraVisualFakeKeyword",
-                            CardKeyword::Ionia => "Ionia",
-                            CardKeyword::Nightfall => "Nightfall",
-                            CardKeyword::PiltoverZaun => "PiltoverZaun",
-                            CardKeyword::Attune => "Attune",
-                            CardKeyword::Daybreak => "Daybreak",
-                            CardKeyword::SilenceIndividualKeyword => "SilenceIndividualKeyword",
-                            CardKeyword::Skill => "Skill",
-                            CardKeyword::Plunder => "Plunder",
-                            CardKeyword::DoubleAttack => "DoubleAttack",
-                            CardKeyword::Vulnerable => "Vulnerable",
-                            CardKeyword::Elusive => "Elusive",
-                            CardKeyword::Stun => "Stun",
-                            CardKeyword::Fated => "Fated",
-                            CardKeyword::BlockElusive => "BlockElusive",
-                            CardKeyword::Fury => "Fury",
-                            CardKeyword::Barrier => "Barrier",
-                            CardKeyword::Immobile => "Immobile",
-                            CardKeyword::Hallowed => "Hallowed",
-                            CardKeyword::Evolve => "Evolve",
-                            CardKeyword::Frostbite => "Frostbite",
-                            CardKeyword::Overwhelm => "Overwhelm",
-                            CardKeyword::QuickAttack => "QuickAttack",
-                            CardKeyword::Tough => "Tough",
-                            CardKeyword::Regeneration => "Regeneration",
-                            CardKeyword::Silenced => "Silenced",
-                            CardKeyword::SpellShield => "SpellShield",
-                            CardKeyword::Lifesteal => "Lifesteal",
-                            CardKeyword::Augment => "Augment",
-                            CardKeyword::Impact => "Impact",
-                            CardKeyword::Scout => "Scout",
-                            CardKeyword::Ephemeral => "Ephemeral",
-                            CardKeyword::Lurk => "Lurk",
-                            CardKeyword::Formidable => "Formidable",
-                            CardKeyword::Challenger => "Challenger",
-                            CardKeyword::Fearsome => "Fearsome",
-                            CardKeyword::CantBlock => "CantBlock",
-                            CardKeyword::Deep => "Deep",
-                            CardKeyword::Flow => "Flow",
-                            CardKeyword::Equipment => "Equipment",
-                            CardKeyword::Unsupported => "<:invaliddeck:1056022952396730438> Unknown",
-                        }).join(", "), false)
-                        .field("Effect", card.localized_description_text.clone(), false)
-                        .footer(|f| f.text(card.localized_flavor_text.clone()));
+                        }
+                    }, true);
+
+                    e.color(match card.supertype {
+                        CardSupertype::Champion => 0x81541f,
+                        _ => match card.rarity {
+                            CardRarity::None => 0x000000,
+                            CardRarity::Common => 0x1e6a49,
+                            CardRarity::Rare => 0x244778,
+                            CardRarity::Epic => 0x502970,
+                            CardRarity::Champion => 0x81541f,
+                        }
+                    });
+
+                    if card.localized_flavor_text.len() > 0 {
+                        e.footer(|f| f.text(card.localized_flavor_text.clone()));
+                    }
 
                     if let Some(art) = card.main_art() {
                         e
