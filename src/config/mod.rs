4c9beea15fcd02e400911a1773230ee62355c3cf
Stefano Pigozzi <me@steffo.eu>
2023-06-22 19:10:54 +0200

    Start work on updating `micronfig`

--- src/config/mod.rs
+++ src/config/mod.rs
@@ -5,61 +5,55 @@
 /// Configuration required by the `exec` feature.
 #[cfg(feature = "exec")]
 pub mod exec {
-	lazy_static::lazy_static! {
-		/// The locale that card data should be downloaded in.
-		/// 
-		/// # Examples
-		/// 
-		/// - `en_US`
-		/// - `it_IT`
-		/// 
-		pub static ref DATA_DRAGON_LOCALE: String = 
-			micronfig::required("DATA_DRAGON_LOCALE");
 
-		/// The set codes for which card data should be downloaded, separated by commas.
-		/// 
-		/// # Examples
-		/// 
-		/// - `set1,set2,set3`
-		/// - `set1`
-		/// - `set1,set2,set3,set4,set5,set6,set6cde,set7`
-		/// 
-		pub static ref DATA_DRAGON_SET_CODES: Vec<String> = 
-			micronfig::required::<String>("DATA_DRAGON_SET_CODES")
-				.split(",").map(|s: &str| s.to_string()).collect();
-	}
+	/// The locale that card data should be downloaded in.
+	///
+	/// # Examples
+	///
+	/// - `en_US`
+	/// - `it_IT`
+	///
+	micronfig::required!(DATA_DRAGON_LOCALE, String);
+
+	/// The set codes for which card data should be downloaded, separated by commas.
+	///
+	/// # Examples
+	///
+	/// - `set1,set2,set3`
+	/// - `set1`
+	/// - `set1,set2,set3,set4,set5,set6,set6cde,set7`
+	///
+	micronfig::required!(DATA_DRAGON_SET_CODES, String);
 }
 
 /// Configuration required by the `jpg` feature.
 #[cfg(feature = "jpg")]
 pub mod jpg {
-	lazy_static::lazy_static! {
-		/// Secret key configured in imgproxy.
-		pub static ref POROXY_KEY: Vec<u8> = micronfig::required("POROXY_KEY");
 
-		/// Salt configured in imgproxy.
-		pub static ref POROXY_SALT: String = micronfig::required("POROXY_SALT");
+	/// Secret key configured in imgproxy.
+	micronfig::required!(POROXY_KEY, Vec<u8>);
 
-		/// URL where imgproxy can be reached at.
-		pub static ref POROXY_HOST: String = micronfig::required("POROXY_HOST");
-	}
+	/// Salt configured in imgproxy.
+	micronfig::required!(POROXY_SALT, String);
+
+	/// URL where imgproxy can be reached at.
+	micronfig::required!(POROXY_HOST, String);
 }
 
 /// Configuration required by the `discord` feature.
 #[cfg(feature = "discord")]
 pub mod discord {
-	lazy_static::lazy_static! {
-		/// ID of the guild where commands should be registered in.
-		/// 
-		/// If not defined, commands are registered globally, and then cached.
-		/// 
-		/// Useful for development, since guild commands are not cached.
-		pub static ref SERENITY_DEV_GUILD_ID: Option<serenity::model::id::GuildId> = micronfig::optional("SERENITY_DEV_GUILD_ID").map(|s: u64| s.into());
 
-		/// The Discord bot token.
-		pub static ref SERENITY_TOKEN: String = micronfig::required("SERENITY_TOKEN");
-		
-		/// The Discord bot app ID.
-		pub static ref SERENITY_APPID: u64 = micronfig::required("SERENITY_APPID");
-	}
+	/// ID of the guild where commands should be registered in.
+	///
+	/// If not defined, commands are registered globally, and then cached.
+	///
+	/// Useful for development, since guild commands are not cached.
+	micronfig::optional!(SERENITY_DEV_GUILD_ID, serenity::model::id::GuildId);
+
+	/// The Discord bot token.
+	micronfig::required!(SERENITY_TOKEN, String);
+
+	/// The Discord bot app ID.
+	micronfig::required!(SERENITY_APPID, u64);
 }
